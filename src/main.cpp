9d698232b3c79e0269fd8f30522718aec627ef24
Amanda <amanda.f.albuquerque@hotmail.com>
2018-04-19 19:58:09 -0300

    comentarios e mudanças em algumas funçoes

--- src/main.cpp
+++ src/main.cpp
@@ -4,28 +4,31 @@
 class Life
 {
     private:
-        using gen_t = size_t;
-        using hash_t = size_t;
+        //tipo usado para guardar valores inteiros positivos
 
-        int _largura, _altura;
-        bool **_matriz;
-        vector<int> _aliveCells;
-        hash_t _hash;
-        gen_t _generation;
+        using gen_t = size_t; //criando alias para size_t
+        using hash_t = size_t; //criando alias para size_t
 
-        std::string generateString(vector<int> &vetor){
-            std::string result;
+        size_t _largura, _altura; //largura e altura são as dimensões da matriz
+        bool **_matriz; // matriz que será representada por ponteiro de ponteiro de booleando
+        vector<int> _aliveCells; //vetor que guarda as coordenadas das celulas vivas
+        hash_t _hash; //chave do vetor aliveCells
+        gen_t _generation; // variavel que faz a contagem de gerações e indica a geração atual
+
+
+        //criar uma string com os valores que tem no vetor que guarda as celulas vivas
+        std::string generateString(const vector<int> &vetor) {
             std::stringstream ss;
 
-            for(int i = 0; i < vetor[i]; i++){
+            // for para colocar os valores do vetor em um fluxo
+            for(int i = 0; i < vetor.size(); i++){
                 ss << vetor[i] << " ";
             }
-            result = ss.str();
-            return result;
+            return ss.str(); //transforma um fluxo em uma string e retorna uma string 
         }
 
     public:
-        Life(std::string &inputFile){
+        Life(std::string &inputFile){ //construtor que recebe o arquivo
             //Paulo implementa
         }
 
@@ -37,7 +40,6 @@ class Life
         //default constructor
         //move constructor
         //move assignment
-        //move constructor
 
         ~Life(){
             for(int i = 0; i < this.altura; i++)
@@ -49,27 +51,26 @@ class Life
             //Amanda e Paulo implementam
         }
 
-        bool isExtinct(){
-            if(_aliveCells.size() == 0){
-                return true;
-            }
+        bool isExtinct() const { // função que checa se só há celulas mortas
+          
+            return (_aliveCells.size() == 0); //retorna verdadeiro se só existir celulas mortas
         }
 
+        //recebe um hash que pega uma string e transforma em size_t
         void createHash(std::hash<std::string> &Hasher){
             _hash = Hasher(generateString(_aliveCells));
         };
-
+        //metodo para adicionar a geração e hash atual dentro de um mapa
         void addToDict(map<hash_t, gen_t> &HashDict){
             HashDict[_hash] = _generation;
         }
 
+        //função booleana que retorna verdadeiro se alguma geração ja aconteceu, e caso contrario, retorna falso 
         bool isStable(map<hash_t, gen_t> &HashDict){
-            if(HashDict.count(_hash) > 0){
-                return true;
-            }else{
-                return false;
-            }
+            return (HashDict.count(_hash) > 0);
         }
+
+        //função para retornar numero da numero da geração que se repete
 };
 
 //operador << pra imprimir
