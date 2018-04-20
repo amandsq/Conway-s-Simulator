3698c8dba1dc7f549da9dea798321494d79e56f7
pauloamed <pauloaugusto992@gmail.com>
2018-04-19 22:44:44 -0300

    Novas funcoes, sobrecargas e comentarios

--- src/main.cpp
+++ src/main.cpp
@@ -4,32 +4,79 @@
 class Life
 {
     private:
-        //tipo usado para guardar valores inteiros positivos
+        using gen_t = size_t; //criando alias para size_t (gen_t)
+        using hash_t = size_t; //criando alias para size_t (hash_t)
 
-        using gen_t = size_t; //criando alias para size_t
-        using hash_t = size_t; //criando alias para size_t
-
-        size_t _largura, _altura; //largura e altura são as dimensões da matriz
+        size_t _height, _width; //as dimensões da matriz
         bool **_matriz; // matriz que será representada por ponteiro de ponteiro de booleando
-        vector<int> _aliveCells; //vetor que guarda as coordenadas das celulas vivas
-        hash_t _hash; //chave do vetor aliveCells
-        gen_t _generation; // variavel que faz a contagem de gerações e indica a geração atual
+        vector<int> _aliveCells; //vetor que guarda as coordenadas (no vetor "grande") das celulas vivas
+        hash_t _hash; //hash do vetor _aliveCells
+        gen_t _generation; // variavel que indica a geração atual
 
 
         //criar uma string com os valores que tem no vetor que guarda as celulas vivas
         std::string generateString(const vector<int> &vetor) {
             std::stringstream ss;
 
-            // for para colocar os valores do vetor em um fluxo
+            // for para colocar os valores do vetor em um stringstream
             for(int i = 0; i < vetor.size(); i++){
                 ss << vetor[i] << " ";
             }
-            return ss.str(); //transforma um fluxo em uma string e retorna uma string 
+            return ss.str(); //transforma um fluxo em uma string e retorna essa string
+        }
+
+        bool **alocateMatrix(size_t _height, size_t _width){
+            bool **ans = new bool*[_height];
+
+            for(int i = 0; i < _height; i++){
+                ans[i] = new bool[_width];
+            }
+            return ans;
         }
 
     public:
-        Life(std::string &inputFile){ //construtor que recebe o arquivo
-            //Paulo implementa
+        /*-------------------------------CONSTRUTORES-E-DESTRUTOR----------------------------*/
+
+        Life(std::string &inputFile){
+            //Declaracao de fluxos e variaveis
+            std::ifstream ifs(inputFile.c_str());
+            std::string line;
+            std::stringstream ss;
+            char alive_char, temp_char;
+
+            //Leitura da primeira linha (altura, largura)
+            std::getline(ifs, line);
+            ss << line;
+            ss >> _height >> _width;
+            ss.clear();
+            //Alocacao de memoria pra matriz
+            _matriz = alocateMatrix(_height, _width);
+
+            //Leitura da segunda linha (caracter vivo)
+            std::getline(ifs, line);
+            ss << line;
+            ss >> alive_char;
+            ss.clear();
+
+            //Leitura da matriz de celulas
+            for(int i = 0; i < _height; i++){
+                std::getline(ifs, line);
+                ss << line;
+                for(int j = 0; j < _width; j++){
+                    ss >> temp_char;
+                    if(temp_char == alive_char){
+                        _matriz[i][j] = true;
+                        _aliveCells.push_back(i * _width + j);
+                    }else{
+                        _matriz[i][j] = false;
+                    }
+                }
+                ss.clear();
+            }
+            ifs.close();
+
+            //Definindo essa geracao como a primeira
+            _generation = 1;
         }
 
         //copy constructor OU gerar com instancia antiga
@@ -47,33 +94,55 @@ class Life
             delete[] _matriz;
         }
 
+        /*-------------------------------SOBRECARGA-DE-OPERADORES----------------------------*/
+
         Life & operator=( const Life& rhs ){
             //Amanda e Paulo implementam
         }
 
-        bool isExtinct() const { // função que checa se só há celulas mortas
-          
-            return (_aliveCells.size() == 0); //retorna verdadeiro se só existir celulas mortas
-        }
+        /*-------------------------------------METODOS---------------------------------------*/
 
-        //recebe um hash que pega uma string e transforma em size_t
+        //recebe um std::hash que cria um hash para uma string
+        //e entao atribui o hash obtido para o hash da configuracao atual
         void createHash(std::hash<std::string> &Hasher){
             _hash = Hasher(generateString(_aliveCells));
-        };
-        //metodo para adicionar a geração e hash atual dentro de um mapa
-        void addToDict(map<hash_t, gen_t> &HashDict){
+        }
+
+        //metodo para adicionar a geração (valor) e hash atual (chave) dentro de um mapa
+        void addToDict(std::map<hash_t, gen_t> &HashDict) const{
             HashDict[_hash] = _generation;
         }
 
-        //função booleana que retorna verdadeiro se alguma geração ja aconteceu, e caso contrario, retorna falso 
-        bool isStable(map<hash_t, gen_t> &HashDict){
-            return (HashDict.count(_hash) > 0);
-        }
+        /*-------------------------------------FUNCOES---------------------------------------*/
 
-        //função para retornar numero da numero da geração que se repete
+        bool isAlive(size_t y, size_t x) const{return _matriz[y][x];}
+
+        //funcao booleana que retorna verdadeiro se nao existirem celulas vivias, falso, caso contrario
+        bool isExtinct() const{return (_aliveCells.size() == 0);}
+
+        //função booleana que retorna verdadeiro se alguma geração ja aconteceu, e caso contrario, retorna falso
+        bool isStable(std::map<hash_t, gen_t> &HashDict) const{return (HashDict.count(_hash) > 0);}
+
+        size_t getHeight() const{return _height;}
+        size_t getWidth() const{return _width;}
+        gen_t getCycleHead(std::map<hash_t, gen_t> &HashDict) const{return HashDict[_hash];}
+        
 };
 
-//operador << pra imprimir
+//sobrecarga de operador << para impressao em stream
+friend ostream& operator<<(ostream& os, const Life& life)
+{
+    size_t height = life.getHeight();
+    size_t width = life.getWidth();
+
+    for(size_t i = 0; i < height; i++){
+        for(size_t j = 0; j < width; j++){
+            if(life.isAlive(i, j)) so << '* ';
+            else so << '. ';
+        }so << '\n';
+    }
+    return os;
+}
 
 int main(int argc, char *argv[])
 {
