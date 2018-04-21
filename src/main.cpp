9a55d8668715e280c1f19670570e91b60ec28888
pauloamed <pauloaugusto992@gmail.com>
2018-04-20 22:46:05 -0300

    Copy e nextGen constructor finalizados. Comentarios. Funcoes auxiliares

--- src/main.cpp
+++ src/main.cpp
@@ -31,34 +31,48 @@ class Life
 
             for(int i = 0; i < _height; i++){
                 ans[i] = new bool[_width];
+                for(int j = 0; j < _width; j++) ans[i][j] = false;
             }
             return ans;
         }
 
         //Funcao que conta as celulas vivas
-        int countAliveCells (size_t line, size_t column){
+        size_t countAliveCells (size_t line, size_t column){
 
-            int count = 0;
+            size_t count(0);
             int analizedLine, analizedColumn;
             for (int i = -1; i < 2; i++) {
+
+                //Propriedades wrapped
                 analizedLine = line + i;
                 if (analizedLine < 0) {
-                    analizedLine = height - 1;
+                    analizedLine = _height - 1;
                 } else if (analizedLine >= _height) {
                     analizedLine = 0;
                 }
-                for (int j = -1; j <= 1; j++) {
+                for (int j = -1; j < 2; j++) {
+
+                    //Propriedades wrapped
                     analizedColumn = column + j;
                     if (analizedColumn < 0) {
                         analizedColumn = _width - 1;
                     } else if (analizedColumn >= _width) {
                         analizedColumn = 0;
                     }
+
+                    //Nao queremos contar com a celula "do meio"
+                    if (analizedLine == line && analizedColumn == column) {
+                        continue;
+                    }
+
+                    //Caso a celula ao redor em questao esteja viva, incrementar o contador
                     if (_matriz[analizedLine][analizedColumn]){
                         count ++;
                     }
                 }
             }
+
+            return count;
         }
 
     public:
@@ -91,11 +105,12 @@ class Life
                 ss << line;
                 for(int j = 0; j < _width; j++){
                     ss >> temp_char;
+
+                    //Caso o caracter em questao seja verdadeiro, adicionar sua coordenada a _aliveCells
+                    //e tornar celula na matriz viva
                     if(temp_char == alive_char){
                         _matriz[i][j] = true;
                         _aliveCells.push_back(std::make_pair(i, j));
-                    }else{
-                        _matriz[i][j] = false;
                     }
                 }
                 ss.clear();
@@ -108,24 +123,74 @@ class Life
 
         //copy constructor OU gerar com instancia antiga
         Life(const Life &otherInstance, bool newGen = true){
-            //Amanda implementa
             if(newGen){//para gerar nova geracao
                 std::cout << "Next generation being created" << std::endl;
-                _height = pastInstance.getHeight();
-                _width = pastInstance.getWidth();
-                _generation = pastInstance.getGeneration();
 
+                //Nova instancia tem matriz com mesmas dimensoes
+                _height = otherInstance._height;
+                _width = otherInstance._width;
+
+                //Estamos criando a proxima geracao
+                _generation = otherInstance._generation + 1;
+
+                //Alocando espaco para matriz
                 _matriz = alocateMatrix(_height, _width);
 
-                //analisar vetor de celulas vivas (da instancia passada)
-                //para cada celula viva (da instancia passada), contar as celulas vivas ao redor
-                //se o contador for 2 ou 3, tornar a celula da instancia (na coordenadas) atual viva
-                //analisar vetor de celulas vizinhas mortas
-                //fazer a mesma coisa para as celulas mortas
+                //Analisando vetor de celulas vivas na instancia passada
+                //a fim de checar se elas continuarao vivas
+                for(int i = 0; i < otherInstance._aliveCells.size(); i++){
+                    //Contando quantas celulas vivas ao redor
+                    size_t counter = otherInstance.countAliveCells
+                            (otherInstance._aliveCells[i].first, otherInstance._aliveCells[i].second);
 
+                    //Caso sejam 2 ou 3 celulas, ela permanece viva
+                    if(counter > 1 && counter < 4){
+                        _matriz[otherInstance._aliveCells[i].first][otherInstance._aliveCells[i].second] = true;
 
-            }else{//para criar copia
+                        //Adicionando celula viva ao vetor _aliveCells da instancia atual
+                        _aliveCells.push_back(otherInstance._aliveCells[i]);
+                    }
+                }
+
+                //Analisando o vetor (da instancia passada) das celulas mortas vizinhas as celulas vivas
+
+                //Declarando vetor temporario para armazenar as coordenadas na instancia passada
+                vector<pair<int,int>> _deadNBCells;
+                //Recuperando as coordenadas
+                otherInstance.getDeadNBCells(_deadNBCells);
+
+                for(int i = 0; i < _deadNBCells.size(); i++){
+                    //Contando quantas celulas vivas ao redor
+                    size_t counter = otherInstance.countAliveCells
+                            (_deadNBCells[i].first, _deadNBCells[i].second);
+
+                    //Caso seja 1 celula, essa celula vai se tornar viva
+                    if(counter == 1){
+                        _matriz[_deadNBCells[i].first][_deadNBCells[i].second] = true;
+
+                        //Adiciando celula viva ao vetor de celulas vivas
+                        _aliveCells.push_back(_deadNBCells[i]);
+                    }
+                }
+
+            }else{
+                //Criando copia da instancia passada
                 std::cout << "Copy constructor being used" << std::endl;
+
+                //Dimensoes, geracao, hash e _aliveCells permanecem os mesmos
+                _height = otherInstance._height;
+                _width = otherInstance._width;
+                _generation = otherInstance._generation;
+                _aliveCells = otherInstance._aliveCells;
+                _hash = otherInstance._hash;
+
+                //Alocando espaco para nova matriz
+                _matriz = alocateMatrix(_height, _width);
+
+                //Copiando elementos da matriz passada para matriz atual
+                for(size_t i = 0; i < _height; i++){
+                    memcpy(_matriz[i], otherInstance.matriz[i], _width);
+                }
             }
         }
 
@@ -194,6 +259,44 @@ class Life
         gen_t getCycleHead(std::map<hash_t, gen_t> &HashDict) const{return HashDict[_hash];}
         gen_t getGeneration() const{return _generation};
 
+        void getDeadNBCells(vector<pair<int,int>> &v){
+            //Declaracao de set temporario, para evitar duplicatas
+            std::set<pair<int,int>> tempSet;
+
+            //Variaveis temporarias para auxilio
+            int analizedLine, analizedColumn;
+            for(int k = 0; k < _aliveCells.size(); k++){
+                for(int i = -1; i <= 1; i++){
+
+                    //Propriedades wrapped
+                    analizedLine = _aliveCells[k].first + i;
+                    if (analizedLine < 0){analizedLine = _height - 1;}
+                    else if(analizedLine >= _height){analizedLine = 0;}
+
+                    for(int j = -1; j <= 1; j++){
+
+                        //Propriedades wrapped
+                        analizedColumn = _aliveCells[k].second + j;
+                        if(analizedColumn < 0){analizedColumn = _width - 1;}
+                        else if(analizedColumn >= _width){analizedColumn = 0;}
+
+                        //A celula em questao eh viva, e nao deve ser considerada
+                        if(_aliveCells[k].first == analizedLine && _aliveCells[k].second == analizedColumn ||
+                                _matriz[analizedLine][analizedColumn])
+                            continue;
+
+                        //Caso ja tenhamos adicionado essa celula ao vetor, nao vamos fazer isso de novo
+                        if(tempSet.count(make_pair(analizedLine, analizedColumn)) == 0){
+                             v.push_back(make_pair(analizedLine, analizedColumn));
+
+                             //Inserindo coordenada ao conjunto, para controle de duplicatas
+                             tempSet.insert(make_pair(analizedLine, analizedColumn));
+                        }
+                    }
+                }
+            }
+        }
+
 };
 
 //sobrecarga de operador << para impressao em stream
@@ -236,8 +339,8 @@ int main(int argc, char *argv[])
 
         //checando se a configuracao atual está extinta
         if(currentGeneration.isExtinct()){
-            //Paulo implementa
-
+            std::cout << "No more cells alive. Can't go on... I'm extinct...\n";
+            std::cout << "Number of generations created: " << currentGeneration.getGeneration() << '\n';
             return 0;
         }
 
@@ -253,7 +356,7 @@ int main(int argc, char *argv[])
         }
 
         //criando a nova geracao, a partir da geracao atual
-        Life nextGeneration(currentGeneration, false);
+        Life nextGeneration(currentGeneration, true);
 
         //atualizando a geracao atual, e invalidando e desalocando a varaivel nextGeneration
         currentGeneration = nextGeneration;
