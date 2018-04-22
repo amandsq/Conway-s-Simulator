4adf547276ac6b1d773a89bb5d33959881225cd2
pauloamed <pauloaugusto992@gmail.com>
2018-04-22 13:52:59 -0300

    working

--- src/main.cpp
+++ src/main.cpp
@@ -372,13 +372,15 @@ std::ostream& operator<< (std::ostream& os, const Life& life)
     for(int i = -1; i <= height; i++){
         for(int j = -1; j <= width; j++){
             if(i == -1 || i == height || j == -1 || j == width){
-                os << "\033[1;44m \033[0m";
+                os << "\033[1;44m  \033[0m";
+                if(j == 0) os << "\033[1;44m \033[0m";
                 continue;
             }
+            if(j == 0) os << "\033[1;47m \033[0m";
             if(life.isAlive(i, j))
-                os << "\033[1;32;42m๏\033[0m";
+                os << "\033[1;47;31m๏ \033[0m";
             else
-                os << "\033[1;47m๏\033[0m";
+                os << "\033[1;47;37m๏ \033[0m";
         }os << "\n\t";
     }os << '\n';
     return os;
