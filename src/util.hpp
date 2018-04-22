0f7bba0e15623aff4ddb767888b1e779ab544593
pauloamed <pauloaugusto992@gmail.com>
2018-04-22 15:20:56 -0300

    removed miscriation

--- src/util.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef UTIL_HEADER
-#define UTIL_HEADER
-
-#include "life.hpp"
-
-//sobrecarga de operador << para impressao em stream
-std::ostream& operator<< (std::ostream& os, const Life& life);
-
-#endif
